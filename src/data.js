391a123d7893dd7a54df7fe4c59fafaefa839841
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-11-15 18:56:09 +0100

    switch to lorem ipsum

--- src/data.js
+++ src/data.js
@@ -3,28 +3,28 @@ export default {
     {
       id: 1,
       name: 'Justin',
-      post: 'Wizkid in the building',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt',
       photo: '/static/img/photo1.jpeg',
       time: '18:08'
     },
     {
       id: 2,
       name: 'David',
-      post: 'Davido\'s performance is dope!',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt',
       photo: '/static/img/photo2.jpg',
       time: '18:28'
     },
     {
       id: 3,
       name: 'Ben',
-      post: 'I think Tiwa should be backing up Wizkid on stage',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt',
       photo: '/static/img/photo1.jpeg',
       time: '18:30'
     },
     {
       id: 4,
       name: 'Larry',
-      post: 'Ypu just have to love MI',
+      post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt',
       photo: '/static/img/photo2.jpg',
       time: '18:46'
     }

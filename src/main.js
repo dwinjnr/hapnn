f41b98874c737af9bc5ceb629080e6e1228e7e19
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-11-16 07:56:04 +0100

    vuefire setup

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueSelectImage from 'vue-select-image'
+import Vuefire from 'vuefire'
+import firebase from './services/firebase'
 import 'material-design-lite/material.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -12,10 +14,14 @@ require('vue-select-image/dist/vue-select-image.css')
 
 Vue.config.productionTip = false
 Vue.use(VueSelectImage)
+Vue.use(Vuefire)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  firebase: {
+    updates: firebase.database.ref('updates').orderByChild('created_at')
+  },
   router,
   template: '<App/>',
   components: { App }

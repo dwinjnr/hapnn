443b842ec2ced225176445182e24343439b519d2
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-11-17 15:17:23 +0100

    feat: cache page skeleton

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,7 +106,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'hapnn',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['dist/**/*.{js,html,css,woff,woff2,ttf,eot,jpeg,jpg,png}'],
       minify: true,
       stripPrefix: 'dist/'
     })
